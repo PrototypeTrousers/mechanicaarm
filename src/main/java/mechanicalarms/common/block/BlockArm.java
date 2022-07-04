6297ab08df039f236ad6b04128c886d0160784db
PrototypeTrousers <salomaoo@gmail.com>
2022-07-03 21:29:23 -0300

    attempt to drop carried item, doesnt.
    microoptimization on draw call

--- src/main/java/mechanicalarms/common/block/BlockArm.java
+++ src/main/java/mechanicalarms/common/block/BlockArm.java
@@ -13,7 +13,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -80,5 +82,17 @@ public class BlockArm extends Block implements ITileEntityProvider {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileArmBasic();
+    }
+
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        TileEntity te = world.getTileEntity(pos);
+        if (te instanceof TileArmBasic) {
+            TileArmBasic tileArmBasic = (TileArmBasic) te;
+            if (!tileArmBasic.getItemStack().isEmpty()) {
+                drops.add(tileArmBasic.getItemStack().copy());
+            }
+        }
+        super.getDrops(drops, world, pos, state, fortune);
     }
 }
